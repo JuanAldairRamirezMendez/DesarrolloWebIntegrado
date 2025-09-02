0dff246688816ee0e988100bb0563bd4df4ebaef
JuanRamirez <u20201597@utp.edu.pe>
2025-09-02 10:55:20 -0500

    agregando la base de datos y creando las entidades relacionadas a las tablas

---
+++ semana3/market/src/main/java/com/TpIntegrado/market/persistence/entity/Cliente.java
@@ -0,0 +1,80 @@
+package com.TpIntegrado.market.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @Column(name = "id")
+    private String idCliente;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellidos") // corregido
+    private String apellidos;
+
+    @Column(name = "celular")
+    private Long celular; // corregido
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+    // Getters y Setters
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Long getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Long celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
